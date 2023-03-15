f056770e8f62d73494d3ec39d7ce57a5b6353de8
tepdock <lksunaylkolodl@gmail.com>
2023-03-15 15:18:09 +0300

    EmailVerification

--- AuthorizationService/AuthorizationService/Controllers/AuthController.cs
+++ AuthorizationService/AuthorizationService/Controllers/AuthController.cs
@@ -1,6 +1,9 @@
 ﻿using Authorization.Domain.DataTransferObject;
 using Authorization.Domain.Models;
 using AutoMapper;
+using CustomExceptionMiddleware.Exceptions;
+using EmailService.Models;
+using EmailService.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,13 +16,15 @@ namespace AuthorizationService.Controllers
         private readonly SignInManager<Account> _signInManager;
         private readonly UserManager<Account> _userManager;
         private readonly IMapper _mapper;
+        private readonly IEmailService _emailService;
 
         public AuthController(SignInManager<Account> signInManager, UserManager<Account> userManager,
-            IMapper mapper)
+            IMapper mapper, IEmailService emailService)
         {
             _signInManager = signInManager;
             _userManager = userManager;
             _mapper = mapper;
+            _emailService = emailService;
         }
 
         /// <summary>
@@ -49,6 +54,13 @@ namespace AuthorizationService.Controllers
                 return BadRequest(ModelState);
             }
 
+            var token = await _userManager.GenerateEmailConfirmationTokenAsync(account);
+            var confirmationLink = Url.Action(nameof(ConfirmEmail), "Auth", 
+                new {token, email = account.Email}, Request.Scheme);
+
+            var message = new Message(new string[] { account.Email }, "Coinfirmation Email", confirmationLink);
+            await _emailService.SendEmail(message);
+
             await _userManager.AddToRolesAsync(account, registerAccountDto.Roles);
             await _signInManager.SignInAsync(account, false);
             return StatusCode(201);
@@ -59,6 +71,17 @@ namespace AuthorizationService.Controllers
         {
             await _signInManager.SignOutAsync();
             return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ConfirmEmail(string token, string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+                throw new BadRequestException("smth went wrong when confirming email");
+
+            var result = await _userManager.ConfirmEmailAsync(user, token);
+            return result.Succeeded ? Ok() : BadRequest(result);
         }
     }
 }
---
+++ AuthorizationService/EmailService/Services/EmailService.cs
@@ -0,0 +1,56 @@
+﻿using CustomExceptionMiddleware.Exceptions;
+using EmailService.Models;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace EmailService.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly EmailConfiguration _emailConfig;
+        public EmailService(EmailConfiguration emailConfig)
+        {
+            _emailConfig = emailConfig;
+        }
+        public async Task SendEmail(Message message)
+        {
+            var emailMessage = CreateEmailMessage(message);
+            await Send(emailMessage);
+        }
+
+        private MimeMessage CreateEmailMessage(Message message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress("email", _emailConfig.From));
+            emailMessage.To.AddRange(message.To);
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
+
+            return emailMessage;
+        }
+
+        private async Task Send(MimeMessage mailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    await client.ConnectAsync(_emailConfig.SmtpServer, _emailConfig.Port, true);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    await client.AuthenticateAsync(_emailConfig.UserName, _emailConfig.Password);
+
+                    await client.SendAsync(mailMessage);
+                }
+                catch
+                {
+                    throw new BadRequestException("smth went wrong while trying to send email");
+                }
+                finally
+                {
+                    await client.DisconnectAsync(true);
+                    client.Dispose();
+                }
+            }
+        }
+    }
+}
